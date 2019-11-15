040869a5b981eb7e2401c6818e44774198ec9991
Ling19329 <ling19329@gmail.com>
2019-11-15 05:05:25 -0500

    add product panel

---
+++ src/components/ProductPanel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="panelBackground">
+    <p class="mb-2 subtitle text-left pl-2">Stationär</p>
+    <v-card class="mx-auto pa-10" outlined>
+      <slot>No slot content defined</slot>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductPanel",
+  props: {
+    panelTitle: String,
+    panelDescription: String,
+    panelBackground: String
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,32 +1,62 @@
 <template>
   <v-layout row wrap justify-center>
     <v-col cols="12" sm="8" xs="12" class="mt-10 pt-10">
-      <v-layout row wrap justify-center="">
-        <v-col cols="12" sm="6">
-          <h2>Ihre betriebliche Gesundheitsförderung</h2>
-          <p class="pb-0 mb-0 mt-4 text-start caption">Geburtsdatum</p>
-          <v-select
-            class="mt-0 pt-0"
-            :items="days"
-            v-model="days[0]"
-            :rules="[v => !!v || '']"
-          ></v-select>
-        </v-col>
-      </v-layout>
+      <v-layout column wrap >
+        <v-row justify="center">
+          <v-col cols="12" sm="6">
+            <h2>Ihre betriebliche Gesundheitsförderung</h2>
+            <p class="pb-0 mb-0 mt-4 text-start caption">Geburtsdatum</p>
+            <v-select
+              class="mt-0 pt-0"
+              :items="days"
+              v-model="days[0]"
+              :rules="[v => !!v || '']"
+            ></v-select>
+          </v-col>
+        </v-row>
 
+        <v-row justify="center">
+          <v-col cols="12" sm="3" xs="6">
+            <product-panel panelBackground="primary">
+              <div>slot content</div>
+            </product-panel>
+          </v-col>
+
+          <v-col cols="12" sm="3" xs="6">
+            <product-panel>
+              <div>slot content</div>
+            </product-panel>
+          </v-col>
+
+          <v-col cols="12" sm="3" xs="6">
+            <product-panel>
+              <div>slot content</div>
+            </product-panel>
+          </v-col>
+
+          <v-col cols="12" sm="3" xs="6">
+            <product-panel>
+              <div>slot content</div>
+            </product-panel>
+          </v-col>
+        </v-row>
+      </v-layout>
       <v-btn large block primary>hello</v-btn>
     </v-col>
   </v-layout>
 </template>
 
 <script>
+import ProductPanel from '@/components/ProductPanel.vue'
 export default {
   name: "Dashboard",
-  components: {},
+  components: {
+    ProductPanel
+  },
   props: {},
   data() {
     return {
-      days: ['01.12.2019']
+      days: ["01.12.2019"]
     };
   },
   methods: {},
